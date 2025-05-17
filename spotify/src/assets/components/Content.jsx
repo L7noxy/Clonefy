c67145da08aea5879a988bbdf44e89a2d9f987d1
GabrielCastanhel <gabrielcastanhel2@gmail.com>
2025-05-17 20:52:30 -0300

    Finalizando o dia

--- spotify/src/assets/components/Content.jsx
+++ spotify/src/assets/components/Content.jsx
@@ -5,7 +5,7 @@ export default function Content() {
     <section className="content">
       <h2>Boa noite</h2>
       <div className="albums">
-        <div className="card">Album 1</div>
+        <div className="card"><img src="./img/scotts.jpg" alt="" /></div>
         <div className="card">Album 2</div>
         <div className="card">Album 3</div>
       </div>
