0ee1208db39d488eda109b2d4e1a14a0f6ad9a1d
GabrielCastanhel <gabrielcastanhel2@gmail.com>
2025-05-18 18:55:04 -0300

    Colocando mais css

--- spotify/src/assets/components/Content.jsx
+++ spotify/src/assets/components/Content.jsx
@@ -5,9 +5,30 @@ export default function Content() {
     <section className="content">
       <h2>Boa noite</h2>
       <div className="albums">
-        <div className="card"><img src="./img/scotts.jpg" alt="" /></div>
-        <div className="card"><img src="./img/juice.jpg" alt="" /></div>
-        <div className="card"><img src="./img/eminem.jpg" alt="" srcset="" /></div>
+        <div className="card">
+          <img src="./img/scotts.jpg" alt="" />
+          <div className='descricao-musica'>
+            <span>
+              The Scotts
+            </span>
+          </div>
+        </div>
+        <div className="card">
+          <img src="./img/juice.jpg" alt="" />
+          <div className='descricao-musica'>
+            <span>
+              juice WRDL
+            </span>
+          </div>
+        </div>
+        <div className="card">
+          <img src="./img/eminem.jpg" alt="" srcset="" />
+          <div className='descricao-musica'>
+            <span>
+              Eminem
+            </span>
+          </div>
+        </div>
       </div>
     </section>
   );
