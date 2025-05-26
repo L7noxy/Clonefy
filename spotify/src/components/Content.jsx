3071ceeb02ba39b80e3ea0fa176b2134af31c379
GabrielCastanhel <gabrielcastanhel2@gmail.com>
2025-05-26 11:09:09 -0300

    Fazendo as ultimas alterações para partir para as funcionalidades

--- spotify/src/components/Content.jsx
+++ spotify/src/components/Content.jsx
@@ -1,19 +1,22 @@
 import './Content.css';
 import './Playlist'
 
-export default function Content({ nome }) {
+export default function () {
   return (
     <section className="content">
-      <h2>Boa noite</h2>
+      <h2>Bem vindo ao clonefy</h2>
       <div className="albums">
         <div className="card">
           <img src="./img/scotts.jpg" alt="" />
           <div className='descricao-musica'>
-            <span>
-              {nome}
-            </span>
+            <h3>
+              The Scotts
+            </h3>
+            <h5>
+              Travis Scott
+            </h5>
           </div>
-        </div> 
+        </div>
       </div>
     </section>
   );
