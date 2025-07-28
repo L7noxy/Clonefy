66481d62ac1e7d510fa6ca9cf8b9420a54283eb3
GabrielCastanhel <gabrielcastanhel2@gmail.com>
2025-07-28 20:41:28 -0300

    Atualizando

--- spotify/src/components/Playlist.jsx
+++ spotify/src/components/Playlist.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 
 import './Playlist.css'
 
-export default function() {
+export default function () {
   function salvar() {
     console.log('salvou')
   }
@@ -10,19 +10,36 @@ export default function() {
   const [nome, setNome] = useState("");
   return (
     <div>
-      <h2>Coloque o nome da sua playlist</h2>
-      <input
-        type="text"
-        value={nome}
-        onChange={(e) => setNome(e.target.value)}
-        placeholder='Colocar o nome da musica'
-      />
+      {/* Seção da Direita */}
+      <aside className="right-section">
+        <img
+          className="track-cover"
+          src="https://upload.wikimedia.org/wikipedia/en/9/99/Travis_Scott_-_Goosebumps.png"
+          alt="goosebumps"
+        />
+        <div className="track-title">goosebumps</div>
+        <div className="artist">Travis Scott</div>
+      </aside>
 
-      {nome && (
-        <h3>Nome da musica: {nome}</h3>
-      )}
+      {/* Player */}
+      <div className="player">
+        <div className="track-info">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/en/9/99/Travis_Scott_-_Goosebumps.png"
+            alt="cover"
+          />
+          <div>
+            <div className="title">goosebumps</div>
+            <div className="artist">Travis Scott</div>
+          </div>
+        </div>
 
-      <button onClick={salvar}>Salvar</button>
+        <div className="player-controls">
+          <button>⏮️</button>
+          <button>⏯️</button>
+          <button>⏭️</button>
+        </div>
+      </div>
     </div>
   )
 }
