abcf975cc852b054f54b80e435fdd6e9de37a1f1
L7noxy <gabrielcastanhel2@gmail.com>
2026-01-15 17:33:45 -0300

    Colocando a parte do adm no frontend

---
+++ spotify/src/router/ProtectedAdmin.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+axios.defaults.withCredentials = true;
+
+export default function ProtectedAdmin({ children }) {
+  const [autorizado, setAutorizado] = useState(null);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const verificar = async () => {
+      try {
+        await axios.get("http://localhost:3000/api/admin/perfil");
+        setAutorizado(true);
+      } catch {
+        setAutorizado(false);
+      }
+    };
+    verificar();
+  }, []);
+
+  if (autorizado === null) return <p>Carregando...</p>;
+
+  if (autorizado === false) {
+    navigate("/login");
+    return null;
+  }
+
+  return children;
+}
--- spotify/src/router/routes.jsx
+++ spotify/src/router/routes.jsx
@@ -4,16 +4,30 @@ import Library from '../pages/Livraria/Library.jsx';
 import Estudos from '../pages/Estudo/Estudos.jsx';
 import Playlist from '../pages/Playlist/Playlist.jsx'
 import Perfil from '../pages/Perfil/Perfil.jsx'
+import ProtectedAdmin from "../router/ProtectedAdmin.jsx";
 
 
 const router = createBrowserRouter([
+
+    { path: "*", element: <h1>404 - Página não encontrada</h1> },
 
     { path: "/", element: <Home /> },
     { path: "/library", element: <Library /> },
     { path: "/estudos", element: <Estudos /> },
     { path: "/playlist", element: <Playlist /> },
     { path: "/perfil", element: <Perfil /> },
-    
+    {
+        path: "/perfil/:id",
+        element: (
+            <ProtectedAdmin>
+                <Perfil />
+            </ProtectedAdmin>
+        )
+
+    },
+
+
+
 
 
 ])
